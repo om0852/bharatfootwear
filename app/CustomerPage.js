d447f4f5e31204b939cff16a58684e9b58bca387
om salunke <omsaunke343@gmail.com>
2024-08-25 08:51:39 +0530

    new items added

--- app/CustomerPage.js
+++ app/CustomerPage.js
@@ -19,57 +19,62 @@ function CustomerPage() {
         price: 769,
         img: "https://m.media-amazon.com/images/I/718bSZO8++L.SX695.jpg",
       },
-  {
+      {
         name: "SPARX mens Sx0678g Sneaker",
         price: 1100,
-        img: "https://m.media-amazon.com/images/I/71iV1K42XJL",
-      },  {
+        img: "https://m.media-amazon.com/images/I/71iV1K42XJL.jpg",
+      },
+      {
         name: "Sparx Men White Running Shoes by Myntra",
-        price: ₹1,550,
+        price: 1550,
         img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcStMMitvV9PFj3pyX9oXdapf4vLYJd3LbsyJF9Hmd13JiHMtBzIbWOZpg6ZjX0VqX3jmA7EiaiFA7J9l_-stEMkZqBrefk_O2p1APwA82RU",
-      },  {
+      },
+      {
         name: "Sparx Men SM-678 Sports Shoes",
-        price: ₹1,349,
+        price: 1349,
         img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSMDLrVAKcsZxsXt_FIg2qagudwTJI70tV67wPCBZzp3JxKhleF-Z1KgaSsPaxFS8V7x4WqAXBP9bX2x22PPQVuP15XPX4bfcvGYMubJ7QRYh594hZVtprU_w",
-      },  {
-        name:"Sparx Men's Synthetic Running Shoes SM-171 ",
-        price:₹1,236,
+      },
+      {
+        name: "Sparx Men's Synthetic Running Shoes SM-171",
+        price: 1236,
         img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR0bKzGKAKTTqy7SO3V7pgryJOSkA0ChQQ2bBmh_iclHZDOJB1AQUUzCmtY6ptQ30dpY1wj63YKNucVvNFVSVPfKFbQuCogIDdy27wbgoVtn_kCBpfl1ZgzCQ",
-      }, 
+      },
+      {
         name: "Sparx Men Sx0676gRunning Shoe",
-        price:₹979,
+        price: 979,
         img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSRstgzs8f8pFDtcxxlbRt50Qr40KbNzw3CIgkRZR0g5jiVsJyI3fCz5yk10uX1MDs3fTJt4CpJqxK4kPvJnyKejLMM0yC8j3fzDt2ntJptVNwxHH7412DW",
-      },  {
-        name: Sparx Mens Sm-438 Running Shoes",
-        price:₹1,304,
+      },
+      {
+        name: "Sparx Mens Sm-438 Running Shoes",
+        price: 1304,
         img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ0zneuUod6FeuWIA7Ce9KAqS9XHYJ5J_CNZqi2nJ9L01RrI6dgpMLBlm-dodyDCbQcu3zOKIJpUpj0Mt0Wtk2eBmj9q1qpAynPDTn5gwS9wmiffJ0COeqYsw",
-      },  {
+      },
+      {
         name: "Men's Sparx Sm-680 Industrial Shoe",
-        price:₹1,123
+        price: 1123,
         img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSAG0rF6l8nZxwO_-qZjM5twvZDDNNv335A0wclb2WZSMz-64wPGBh6J-dyfWo90wjVQFPPlKbeuvTPnlgLzdExSS0aD9awzmV-hpAELWDs82bVvLBXQhDl",
-      },  {
+      },
+      {
         name: "Sparx Men's Synthetic Running Shoes",
-        price: ₹1,566,
-        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSzk12DCtep3rkEs-8v5_8vvSzhTUsSAILpF1NB7GTX9J94kDEDCepTZ0kcXVFSMlKmfp89CGdMrGevG1SOU19qEci2CzBtpwWlPecqDGehWtDU0teAhtjEZw";
-	},
+        price: 1566,
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSzk12DCtep3rkEs-8v5_8vvSzhTUsSAILpF1NB7GTX9J94kDEDCepTZ0kcXVFSMlKmfp89CGdMrGevG1SOU19qEci2CzBtpwWlPecqDGehWtDU0teAhtjEZw",
+      },
     ],
     Adidas: [
       {
         name: "Adidas Mens Drogo Running Shoes",
         price: 1725,
-        img: " https://m.media-amazon.com/images/I/61hGNA5utrL._SX500_.jpg",
-      }
-{,
-      {{
-        name: " Adidas Men's Clinch-X M Running Shoe”
- ",        price: 1489,
-        img: " https://m.media-amazon.com/images/I/71sfbi31lhL._SX695_.jpg ",
+        img: "https://m.media-amazon.com/images/I/61hGNA5utrL._SX500_.jpg",
       },
-    ],
+      {
+        name: "Adidas Men's Clinch-X M Running Shoe",
+        price: 1489,
+        img: "https://m.media-amazon.com/images/I/71sfbi31lhL._SX695_.jpg",
+      },
       {
         name: "Adidas Mens Runcrypt M Running Shoe",
         price: 1222,
-        img: " https://m.media-amazon.com/images/I/715nErX-QPL._SY500_.jpg ",
+        img: "https://m.media-amazon.com/images/I/715nErX-QPL._SY500_.jpg",
       },
       {
         name: "adidas Mens Fuseo M Running Shoe",
@@ -98,55 +103,62 @@ function CustomerPage() {
       },
       {
         name: "adidas Mens Astoundrun M Running Shoe",
-        price: 1,826,
+        price: 1826,
         img: "https://m.media-amazon.com/images/I/71WJ9dlnSRL._SX695_.jpg",
       },
       {
         name: "adidas mens Amalgo M Running Shoe",
-        price:1977,
+        price: 1977,
         img: "https://m.media-amazon.com/images/I/81ATe15IyHL._SX695_.jpg",
       },
-
+    ],
     Nike: [
       {
-        name: "Nike Mens Revolution 7 Men's Road Running ShoesRunning Shoes",
+        name: "Nike Mens Revolution 7 Men's Road Running Shoes",
         price: 3120,
         img: "https://m.media-amazon.com/images/G/31/gno/sprites/nav-sprite-global-1x-reorg-privacy.CB600086755.png",
       },
       {
         name: "Nike Mens Quest 5 Running Shoe",
         price: 2599,
-        img: "https://m.media-amazon.com/images/I/71atP1wteYL",
+        img: "https://m.media-amazon.com/images/I/71atP1wteYL._SY500_.jpg",
       },
- {
+      {
         name: "Nike Mens Revolution 5 (4e) Running Shoe",
         price: 2100,
-        img: "https://m.media-amazon.com/images/I/81Wy5uIFdmL",
-      }, {
+        img: "https://m.media-amazon.com/images/I/81Wy5uIFdmL._SX500_.jpg",
+      },
+      {
         name: "Nike Mens City Rep Tr Men's Workout Shoes Sneaker",
         price: 3100,
-        img: "https://m.media-amazon.com/images/I/71lyQrHr74L.SX575.jpg",
-      }, {
+        img: "https://m.media-amazon.com/images/I/71lyQrHr74L._SX575_.jpg",
+      },
+      {
         name: "Nike mens Court Vision Lo Nn Ess Running Shoes",
         price: 2599,
-        img: "https://m.media-amazon.com/images/I/71lyQrHr74L",
-      }, {
+        img: "https://m.media-amazon.com/images/I/71lyQrHr74L._SX500_.jpg",
+      },
+      {
         name: "Nike mens Nike Air Max Impact 4 Running Shoes",
         price: 3100,
-        img: "https://m.media-amazon.com/images/I/71subBFxFLL",
-      }, {
-        name: "NIKE Men's Journey Run ShoesNike Free Run",
+        img: "https://m.media-amazon.com/images/I/71subBFxFLL._SX500_.jpg",
+      },
+      {
+        name: "NIKE Men's Journey Run Shoes",
         price: 2500,
-        img: "https://m.media-amazon.com/images/I/61IswKI4gEL.SY500.jpg",
-      }, {
-        name: "Nike Mens Air Zoom Structure Running Shoes 25Nike",
+        img: "https://m.media-amazon.com/images/I/61IswKI4gEL._SY500_.jpg",
+      },
+      {
+        name: "Nike Mens Air Zoom Structure Running Shoes 25",
         price: 3155,
-        img: "https://m.media-amazon.com/images/I/81aDGWhNpcL",
-      }, {
+        img: "https://m.media-amazon.com/images/I/81aDGWhNpcL._SX500_.jpg",
+      },
+      {
         name: "Nike Mens M Mc Trainer 2 Running Shoe",
-        price: 100,
-        img: "https://m.media-amazon.com/images/I/71+qZWsDB3L",
-      }, {
+        price: 3225,
+        img: "https://m.media-amazon.com/images/I/71ZWpggG2oL._SY500_.jpg",
+      },
+      {
         name: "NIKE mens Downshifter 13 Running shoe",
         price: 3129,
         img: "https://m.media-amazon.com/images/I/71dhfPFeOtL",
@@ -179,54 +191,53 @@ function CustomerPage() {
     Lancer: [
       {
         name: "LANCER Men's Sports & Outdoor Running Shoes",
-        price: ₹944,
-        img: "https://m.media-amazon.com/images/I/71HQX5LqVLL",
+        price: 944,
+        img: "https://m.media-amazon.com/images/I/71HQX5LqVLL.jpg",
       },
       {
         name: "LANCER Mens Spyder-1 Running Shoe",
-        price: ₹899,
-        img: "https://m.media-amazon.com/images/I/71laV4kIaeL",
+        price: 899,
+        img: "https://m.media-amazon.com/images/I/71laV4kIaeL.jpg",
       },
-
       {
         name: "Lancer Mens INDUS-12 Black Maroon Running Shoe - 6 UK (INDUS-12BLK-MRN-6_Black Maroon_6 UK)",
-        price:₹699 ,
-        img: "https://m.media-amazon.com/images/I/71oQXRtEV1S",
+        price: 699,
+        img: "https://m.media-amazon.com/images/I/71oQXRtEV1S.jpg",
       },
       {
         name: "LANCER Men's Running Sports Shoes ACTIVE-47",
-        price: ₹574,
-        img: "https://m.media-amazon.com/images/I/61lFdCdRYAL",
+        price: 574,
+        img: "https://m.media-amazon.com/images/I/61lFdCdRYAL.jpg",
       },
       {
         name: "Lancer Men's Sports Running Shoes Indus-251",
-        price: ₹564,
-        img: "https://m.media-amazon.com/images/I/61hwBsknJSL",
+        price: 564,
+        img: "https://m.media-amazon.com/images/I/61hwBsknJSL.jpg",
       },
       {
         name: "LANCER mens Indus-215 Sports Shoes",
-        price: ₹564,
-        img: "https://m.media-amazon.com/images/I/71Uqcsss4lL",
+        price: 564,
+        img: "https://m.media-amazon.com/images/I/71Uqcsss4lL.jpg",
       },
       {
         name: "LANCER Mens Active-115 Platform",
-        price: ₹694,
-        img: "https://m.media-amazon.com/images/I/71UtKYmAMlL",
+        price: 694,
+        img: "https://m.media-amazon.com/images/I/71UtKYmAMlL.jpg",
       },
       {
         name: "LANCER Men's Lace Up Sports Running Outdoor Shoes",
-        price: ₹749,
-        img: "https://m.media-amazon.com/images/I/71CJIlTo5FL",
+        price: 749,
+        img: "https://m.media-amazon.com/images/I/71CJIlTo5FL.jpg",
       },
       {
         name: "Lancer mens Wonder-10nbl-wht Running Shoes",
-        price: ₹564,
-        img: "https://m.media-amazon.com/images/I/71Lo1Ywt16L",
+        price: 564,
+        img: "https://m.media-amazon.com/images/I/71Lo1Ywt16L.jpg",
       },
       {
         name: "LANCER Men La-107 Tan Suede Sneakers",
-        price:₹569,
-        img: "https://m.media-amazon.com/images/I/71E6zhtrrcL",
+        price: 569,
+        img: "https://m.media-amazon.com/images/I/71E6zhtrrcL.jpg",
       },
     ],
     Acolight: [
@@ -256,112 +267,118 @@ function CustomerPage() {
     Campus: [
       {
         name: "Campus Men's Tyson PRO Running Shoes",
-        price: ₹844,
-        img: "https://www.amazon.in/Campus-Crysta-PRO-T-BLU-Running/dp/B09RBFQ74N?ref_=ast_sto_dp&th=1&psc=1",
+        price: 844,
+        img: "https://m.media-amazon.com/images/I/71XV8y4A8tL.jpg",
       },
       {
         name: "Campus Men's S-Cross PRO Walking Shoes",
-        price:₹899,
-        img: "https://www.amazon.in/Campus-S-CROSS-PRO-walking-shoes/dp/B09RBFXKKQ?ref_=ast_sto_dp&th=1&psc=1",
+        price: 899,
+        img: "https://m.media-amazon.com/images/I/71z8ncBFrhL.jpg",
       },
-  {
+      {
         name: "Campus Mens Ignite ProRunning Shoes",
-        price:₹802,
-        img: "https://www.amazon.in/Campus-Ignite-PRO-Running-Shoes/dp/B09VH86YT6?ref_=ast_sto_dp&th=1&psc=1",
+        price: 802,
+        img: "https://m.media-amazon.com/images/I/71XV8y4A8tL.jpg",
       },
       {
         name: "Campus Men's MAGNITE Running Shoes",
-        price: ₹699,
-        img: "https://m.media-amazon.com/images/I/71XV8y4A8tL",
-      },  {
+        price: 699,
+        img: "https://m.media-amazon.com/images/I/71XV8y4A8tL.jpg",
+      },
+      {
         name: "Campus mens Bull Pro Running Shoes",
-        price: ₹797,
-        img: "https://www.amazon.in/Campus-PRO-Running-Shoes-India/dp/B09RBFLCKT?ref_=ast_sto_dp&th=1&psc=1",
+        price: 797,
+        img: "https://m.media-amazon.com/images/I/71XV8y4A8tL.jpg",
       },
       {
         name: "Campus Men's XING Running Shoes",
-        price:₹ 789,
-        img: "https://m.media-amazon.com/images/I/71z8ncBFrhL",
-      },  {
+        price: 789,
+        img: "https://m.media-amazon.com/images/I/71z8ncBFrhL.jpg",
+      },
+      {
         name: "Campus Men's DREAMPLEX Walking Shoes",
-        price:₹1109,
-        img: "https://www.amazon.in/Campus-DREAMPLEX-D-Gry-Running-Shoes/dp/B09QXFT5QQ?ref_=ast_sto_dp&th=1&psc=1,
+        price: 1109,
+        img: "https://m.media-amazon.com/images/I/71z8ncBFrhL.jpg",
       },
       {
         name: "Campus Mens Rodeo ProRunning Shoe",
-        price:₹897,
-        img: "https://m.media-amazon.com/images/I/61tdxAZiO2L",
-      },  {
+        price: 897,
+        img: "https://m.media-amazon.com/images/I/61tdxAZiO2L.jpg",
+      },
+      {
         name: "Campus Men's Camp Smart Walking Shoes",
-        price:₹1190,
-        img: "https://m.media-amazon.com/images/I/51W00IBurOL",
+        price: 1190,
+        img: "https://m.media-amazon.com/images/I/71z8ncBFrhL.jpg",
       },
       {
         name: "Campus Men's Costa PRO Walking Shoes",
-        price:₹688,
-        img: "https://m.media-amazon.com/images/I/615ztiLDXkL",
+        price: 688,
+        img: "https://m.media-amazon.com/images/I/71z8ncBFrhL.jpg",
       },
     ],
     Paragon: [
       {
         name: "Paragon K11241G Men Formal Shoes Smart & Sleek Design",
-        price: ₹639,
-        img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTKSRWKZ1wHtujqx9lrL_OdKHd58ohY_G2FL4PQpK3TTL4yzPDNSWwxQo7qWpLLj8aiuwh58G3cRifCnoF0MoOpH8BLyTeKJMaub7c-v7u4LKtq7z30fiZrgmM,
+        price: 639,
+        img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTKSRWKZ1wHtujqx9lrL_OdKHd58ohY_G2FL4PQpK3TTL4yzPDNSWwxQo7qWpLLj8aiuwh58G3cRifCnoF0MoOpH8BLyTeKJMaub7c-v7u4LKtq7z30fiZrgmM.jpg",
       },
       {
         name: "Paragon K11237G Men Loafers Stylish Walking Outdoor Shoes Daily & Occasion Wear",
-        price: ₹911,
-        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTcr_GttKjiLuUUJpddDlRDAi4roeD_n38b2YB8gV5ozeQ5b1hHdVXZPhlLX_0sN6inh3JhlJPrFR7xEnuBsMMUrEAYzbCWSCzN-IRKOQ_QYvqogRfmc37IDA",
+        price: 911,
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTcr_GttKjiLuUUJpddDlRDAi4roeD_n38b2YB8gV5ozeQ5b1hHdVXZPhlLX_0sN6inh3JhlJPrFR7xEnuBsMMUrEAYzbCWSCzN-IRKOQ_QYvqogRfmc37IDA.jpg",
       },
-  {
+      {
         name: "Paragon Men Mesh Walking Sports Shoes by Myntra",
-        price: ₹749,
-        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSumVu9-NHRRjZXfKqghgN5bn-PY-IEnM15i7_wldw1TkoiNb5dTlAhKTF7ozCLH9wwtdmgYerAIwitTy7RXvcaqorCStIwiA7BcAtdlL3vfk5HgGBlCHqsBA",
+        price: 749,
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSumVu9-NHRRjZXfKqghgN5bn-PY-IEnM15i7_wldw1TkoiNb5dTlAhKTF7ozCLH9wwtdmgYerAIwitTy7RXvcaqorCStIwiA7BcAtdlL3vfk5HgGBlCHqsBA.jpg",
       },
       {
         name: "Paragon Eeken KE1216G Comfortable Daily Outdoor Walking Shoes for Men",
-        price: ₹929,
-        img: "src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSxdfXDXqxZIazd_0PFqQfoGvonP0KyWPwKU5nNAKurwPCuivR5bmNkpx4_Ax64c9vMJMZo3UYJmjuT-6Yuct3K9hYF7DI8LQovUQyzXZFSE54fiJTsWt-ZcJM";
-      },  
-{
-        name: "Men's Paragon K1214G Casual Shoes Stylish Walking Outdoor Shoes for Everyday Wear";
-        price:₹699,
-        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQCdj-eO043nad73vS6EhzSF8y6POVkFbDm3dwjXmFlxh-U7FSYdFGX07IYEdhZn8wb9fz88AFsN1QVS5uo3wPBrTRMtbte8Jg8GeLfg-aWwCzq7vH-XYzAPBA",
+        price: 929,
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSxdfXDXqxZIazd_0PFqQfoGvonP0KyWPwKU5nNAKurwPCuivR5bmNkpx4_Ax64c9vMJMZo3UYJmjuT-6Yuct3K9hYF7DI8LQovUQyzXZFSE54fiJTsWt-ZcJM.jpg",
+      },
+      {
+        name: "Men's Paragon K1214G Casual Shoes Stylish Walking Outdoor Shoes for Everyday Wear",
+        price: 699,
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQCdj-eO043nad73vS6EhzSF8y6POVkFbDm3dwjXmFlxh-U7FSYdFGX07IYEdhZn8wb9fz88AFsN1QVS5uo3wPBrTRMtbte8Jg8GeLfg-aWwCzq7vH-XYzAPBA.jpg",
       },
       {
         name: "Paragon K1020g Casual Shoes Stylish Walking Outdoor Shoes for Men Everyday Wear",
-        price:₹682,
-        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQwOQrxTHBoRarjwF-JC_nPmyMJVURRJCYZPoEZVtM5QryXCkVsPvBXGHdOC9GC0unHRcbwgA4EIVWUqJThiuQp_ylZIQgBsRyOf1J_2w8uT1APu_ChEXkDqA",
-      },  {
+        price: 682,
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQwOQrxTHBoRarjwF-JC_nPmyMJVURRJCYZPoEZVtM5QryXCkVsPvBXGHdOC9GC0unHRcbwgA4EIVWUqJThiuQp_ylZIQgBsRyOf1J_2w8uT1APu_ChEXkDqA.jpg",
+      },
+      {
         name: "Paragon PUK3502GS Men Sports Shoes Walking Running Training Cricket Gym Shoes",
-        price:₹779,
-        img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRRbwcRkF5BQBBGGu1GqSg34aplWtyMpC5flx0l7XiNC0mxLrLVVJu_7oDb2b3BZWY6S0mHK7vYukw6Tj5gmuzO6Kt3Ay_RpS3H3y-Nnh-I24xCXU2CnHja6g",
+        price: 779,
+        img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRRbwcRkF5BQBBGGu1GqSg34aplWtyMpC5flx0l7XiSKM7n-DlZes8cbxuDN0HFH6NT6Px7TzcbQWSuAY9MojntbIxdFOV3Pxk43pSgOVSvNThEGP_fyOh2B7s.jpg",
       },
       {
-        name: "Paragon K1221G Men Walking Running Training Cricket Gym Sports Shoes",
-        price: ₹942,
-        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTnq8ljDGsVfMOrBwo3JeFSoBhVPFoiCciZFYUYDs7lC_On5uYgJ-IBXjQZGs534GA7Kf3WQNJfVYgMFJagmBTGFX4srjmiSBcD2I7hgibD7qImL0voZwuL_w",
-      },  {
-        name: "Paragon PUK1230G Men Casual Shoes Stylish Walking Outdoor Shoes",
-        price: ₹1,023,
-        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQvpQZVRioD1aJX6DrlyvqhnXteEwJ3LxSncRN7DjK9EPmvo_oWFkkc6ZqMyv7W4fsO0KrbyxSivG1VBPwKvoeKmXk8HvSAjvlTcvhV6p35xljRyJTsE_9A",
+        name: "Paragon PU3516G Men Sports Shoes Trendy Walking Shoes Daily Wear Shoes",
+        price: 658,
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQXz2uMgVsA59hy13OZVxkTf1v-TM0TYhVzib84CgdRSZ0yNQSRoaX7LKn6nXbKE8d5BrDpvSntkldbrzWSLEg9z24VG6XN2VROcm1uGQGxSESGohcFcM7wopg.jpg",
       },
       {
-        name: "Paragon Men's Casual Shoes | Latest Style with Cushioned Insole & Sturdy Construction",
-        price: ₹973,
-        img: "https://images.meesho.com/images/products/345563812/ovqut_512.webp",
+        name: "Men's Stylish Sports Shoes by Paragon - White and Grey",
+        price: 799,
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQgpP30ix5pCfbJnd9LU6IMvjqL9X0IKdPQ3elw8kddB5YlvOHREiWvgqFZ1ahEZ32hR4XL4_9wCOVc3GBYZvYyn_GnBPeId8JtTk4Z1vTboRC9N7GJlKiS-A.jpg",
+      },
+      {
+        name: "Men's Stylish Sports Shoes by Paragon - Black",
+        price: 890,
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRUOWc60Pxoxpzw54RFEBOvqG13kl9phWFLsH5zxhUmCHkm7MphLuwU8WzN3MvFy5SLknJGrMrKKyNQwoFCmkk1eJ43IbRTiX3Koho1JhJm_nj4Wfwn87IbRII.jpg",
       },
     ],
-  }; 
-const [companies,setCompaines]=useState(companiesList)
+  };
+
+  const [companies, setCompaines] = useState(companiesList);
   useEffect(() => {
-     const companiesList1 = Object.entries(companiesList)
+    const companiesList1 = Object.entries(companiesList)
       .filter(([key]) => key.toLowerCase().startsWith(inp.toLowerCase()))
       .reduce((acc, [key, value]) => {
         acc[key] = value;
         return acc;
       }, {});
-      setCompaines(companiesList1)
+    setCompaines(companiesList1);
   }, [inp]);
 
   const [showProfile, setShowProfile] = useState(false);
