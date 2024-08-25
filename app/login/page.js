e705ea5c1dea73c93b2d14c1c152ae0ec3297dce
om salunke <omsaunke343@gmail.com>
2024-08-25 15:11:25 +0530

    login auth problem solve

--- app/login/page.js
+++ app/login/page.js
@@ -15,15 +15,21 @@ function Page({ onLogin }) {
       .post("/api/login", { email, password, role })
       .then((res) => {
         alert(res.data.message);
-        Cookie.set("role", res.data.role);
-        router.push("/");
+        if (res.data.message != "Login Failed") {
+          Cookie.set("role", res.data.role);
+          router.push("/");
+        }
       })
       .catch((err) => console.log(err));
   };
 
   return (
     <div className="login-container">
-              <img className="mx-auto" width={50} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZP-fS3vscpaIWZYdvmnNpf3iHfEJpoU3K7IdwQJVM1zrzv-OYXWV0wxNwmiK0MXH3oig&usqp=CAU"/>
+      <img
+        className="mx-auto"
+        width={50}
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZP-fS3vscpaIWZYdvmnNpf3iHfEJpoU3K7IdwQJVM1zrzv-OYXWV0wxNwmiK0MXH3oig&usqp=CAU"
+      />
 
       <h2>Login</h2>
       <input
